0e84fd5492be3460414da0deae117677caabc168
Oleg <konevov@gmail.com>
2021-06-21 09:59:21 +0300

    Changed Task 04 and 05

--- M02T05/main.go
+++ M02T05/main.go
@@ -30,7 +30,6 @@ func main() {
 	a := []string{"11", "22", "33"}
 	x := "33"
 	fmt.Println("Result of the contains function:", contains(a, x))
-	//fmt.Println("Result of the getMax function:", getMax(1, 2, 3, 4, 5, 23, 7, 6))
 	fmt.Println("Result of the getMax function:", getMax(-2, -4))
-
+	// Changed solution
 }
